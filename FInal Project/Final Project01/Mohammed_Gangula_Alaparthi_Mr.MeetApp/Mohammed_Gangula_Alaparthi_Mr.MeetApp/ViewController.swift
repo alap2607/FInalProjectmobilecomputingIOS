3fea1b0992d13637ceda503dc0b8218fd8b30653
Navya Sri Alaparthi <89713499+alap2607@users.noreply.github.com>
2022-04-25 16:45:34 -0500

    Add files via upload

---
+++ FInal Project/Final Project01/Mohammed_Gangula_Alaparthi_Mr.MeetApp/Mohammed_Gangula_Alaparthi_Mr.MeetApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Mohammed_Gangula_Alaparthi_Mr.MeetApp
+//
+//  Created by Navya Alaparthi on 4/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+   
+    @IBOutlet weak var tableCell: UITableViewCell!
+    
+}
+
---
+++ FInal Project/Mohammed_Gangula_Alaparthi_DrMeetApp/Mohammed_Gangula_Alaparthi_DrMeetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mohammed_Gangula_Alaparthi_DrMeetApp
+//
+//  Created by Navya Alaparthi on 4/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
